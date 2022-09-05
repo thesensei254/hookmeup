6b652316832ebc0e3b9c4ec3c37ea9910e37db81
thesensei254 <ngondomc@gmail.com>
2022-09-05 17:17:21 -0500

    Scaffolding files and fxns to use

---
+++ src/components/Game.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Board from './Board';
+
+
+const Game = () => {
+
+  const handleClick = () => {
+
+  }
+
+  const jumpTo = () => {
+
+  }
+
+  const renderMoves = () => {
+    
+  }
+ 
+  return (
+    <Board onClick={handleClick} />
+  )
+}
+
+export default Game
