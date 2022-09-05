3f1fb646e5c989b42c4a100517a5b1460f67c745
thesensei254 <ngondomc@gmail.com>
2022-09-05 17:28:51 -0500

    Board styling on square and board components

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,8 +1,18 @@
 import React from 'react'
 import Square from './Square'
 
+const style = {
+  border: '4px solid darkblue',
+  borderRadius: '10px',
+  width: '300px',
+  height: '300px',
+  margin: '0 auto',
+  display: 'grid',
+  gridTemplate: 'repeat(3, 1fr)/repeat(3, 1fr)'
+}
+
 const Board = ({squares, onClick}) => (
-  <div>
+  <div style={style}>
     <Square value="1" onClick={() => onClick("Dummy value")} />
     <Square value="2" onClick={() => onClick("Dummy value")} />
     <Square value="3" onClick={() => onClick("Dummy value")} />
